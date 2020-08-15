6da2331ee07ee3f28fa1fd86aeca9d95015be01f
Lucas Botelho <lucas.vbotelho83@gmail.com>
2020-08-15 20:46:27 -0300

    final version

--- app.js
+++ app.js
@@ -8,62 +8,23 @@ var fs = require('fs');
 // invocation we used manually before
 
 async function setupServer(){
-    if(!fs.existsSync("./server")){
-        try{
-            await exec('mkdir server')
-            await exec('wget -O server/minecraft_server.jar https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar');
-            await setupJava()
-            await setupEULA()
-        }catch(err){
-            console.log(err)
-            return
-        }
-    }
-    var minecraftServerProcess = spawn('java', [
-        '-Xmx1024M',
-        '-Xms1024M',
-        '-jar',
-        'minecraft_server.jar',
-        'nogui'
-    ],{
-        cwd : "server"
-    });
-    
-    // Listen for events coming from the minecraft server process - in this case,
-    // just log out messages coming from the server
-    minecraftServerProcess.stdout.on('data', log);
-    minecraftServerProcess.stderr.on('data', log);
-
-
-    // Create an express web app that can parse HTTP POST requests
-    var app = require('express')();
-    app.use(require('body-parser').urlencoded({
-        extended:false
-    }));
-
-    // Create a route that will respond to a POST request
-    app.get('/command', function(request, response) {
-        // Get the command from the HTTP request and send it to the Minecraft
-        // server process
-        console.log(request.query);
-        var command = request.query["Body"];
-        minecraftServerProcess.stdin.write(command+'\n');
-
-        // buffer output for a quarter of a second, then reply to HTTP request
-        var buffer = [];
-        var collector = function(data) {
-            data = data.toString();
-            buffer.push(data.split(']: ')[1]);
-        };
-        minecraftServerProcess.stdout.on('data', collector);
-        setTimeout(function() {
-            minecraftServerProcess.stdout.removeListener('data', collector);
-            response.send(buffer.join(''));
-        }, 250);
-    });
-
-    // Listen for incoming HTTP requests on port 3000
-    app.listen(3002);
+    return new Promise(
+        async function(resolve, reject) { 
+            if(!fs.existsSync("./server")){
+                try{
+                    await exec('mkdir server')
+                    await exec('wget -O server/minecraft_server.jar https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar');
+                    await setupJava()
+                    await setupEULA()
+                    resolve()
+                }catch(err){
+                    console.log(err)
+                    reject()
+                }
+            }else{
+                resolve()
+            }
+        })
 }
 
 function setupJava(){
@@ -113,4 +74,57 @@ function log(data) {
     process.stdout.write(data.toString());
 }
 
-setupServer()
+setupServer().then(data => {
+    var minecraftServerProcess = spawn('java', [
+        '-Xmx1024M',
+        '-Xms1024M',
+        '-jar',
+        'minecraft_server.jar',
+        'nogui'
+    ],{
+        cwd : "server"
+    });
+    
+    // Listen for events coming from the minecraft server process - in this case,
+    // just log out messages coming from the server
+    minecraftServerProcess.stdout.on('data', log);
+    minecraftServerProcess.stderr.on('data', log);
+    
+    // Create an express web app that can parse HTTP POST requests
+    var app = require('express')();
+    app.use(require('body-parser').urlencoded({
+        extended:false
+    }));
+    
+    // Create a route that will respond to a POST request
+    app.get('/command', function(request, response) {
+        // Get the command from the HTTP request and send it to the Minecraft
+        // server process
+        console.log(request.query);
+        var command = request.query["Body"];
+        minecraftServerProcess.stdin.write(command+'\n');
+    
+        // buffer output for a quarter of a second, then reply to HTTP request
+        var buffer = [];
+        var collector = function(data) {
+            data = data.toString();
+            buffer.push(data.split(']: ')[1]);
+        };
+        minecraftServerProcess.stdout.on('data', collector);
+        setTimeout(function() {
+            minecraftServerProcess.stdout.removeListener('data', collector);
+            response.send(buffer.join(''));
+        }, 250);
+    });
+    
+    app.get('/', function(request, response) {
+        response.send('Minecraft Server')
+    });
+    
+    // Listen for incoming HTTP requests on port 3000
+    var server = app.listen(3002);
+    minecraftServerProcess.on('close', function (code) {
+        console.log("Server Has Been Closed")
+        server.close()
+    });
+})
