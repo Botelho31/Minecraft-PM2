89f8ed0fcaf2b1672cb708bac2fb012b6921ab47
Botelho31 <lucas.vbotelho83@gmail.com>
2023-12-05 19:39:38 -0300

    feat: add prints if its not there

--- scripts/setup-pi.sh
+++ scripts/setup-pi.sh
@@ -1,24 +1,44 @@
 # /bin/bash
 
 # add usb-storage.quirks=152d:0578:u to the beginning of the file /boot/cmdline.txt
-echo "usb-storage.quirks=152d:0578:u " > temp.txt
-cat /boot/cmdline.txt >> temp.txt
-sudo mv temp.txt /boot/cmdline.txt
 
-sudo echo `
-interface eth0
-static ip_address=192.168.0.9/24
-static routers=192.168.0.1
-static domain_name_servers=192.168.0.1 8.8.8.8
+# check if the line is already there
 
-interface wlan0
-static ip_address=192.168.0.9/24
-static routers=192.168.0.1
-static domain_name_servers=192.168.0.1 8.8.8.8` >> /etc/dhcpcd.conf
+if grep -q "usb-storage.quirks=152d:0578:u" /boot/cmdline.txt; then
+    echo "usb-storage.quirks=152d:0578:u is already in the file"
+else
+    echo "usb-storage.quirks=152d:0578:u is not in the file"
+    echo "usb-storage.quirks=152d:0578:u " > temp.txt
+    cat /boot/cmdline.txt >> temp.txt
+    sudo mv temp.txt /boot/cmdline.txt
+fi
 
-sudo echo "
-network={
-        ssid="RALU_NET_5G"
-        psk=0815233128
-        psk_mgmt=WPA-PSK
-}" >> /etc/wpa_supplicant/wpa_supplicant.conf
+# check if the line is already there
+
+if grep -q "192.168.0.9/24" /etc/dhcpcd.conf; then
+    echo "The static ip is already in the file"
+else 
+    echo "The static ip is not in the file"
+    sudo echo `
+    interface eth0
+    static ip_address=192.168.0.9/24
+    static routers=192.168.0.1
+    static domain_name_servers=192.168.0.1 8.8.8.8
+
+    interface wlan0
+    static ip_address=192.168.0.9/24
+    static routers=192.168.0.1
+    static domain_name_servers=192.168.0.1 8.8.8.8` >> /etc/dhcpcd.conf
+fi
+
+if grep -q "RALU_NET_5G" /etc/wpa_supplicant/wpa_supplicant.conf; then
+    echo "The wifi is already in the file"
+else 
+    echo "The wifi is not in the file"
+    sudo echo "
+    network={
+            ssid="RALU_NET_5G"
+            psk=0815233128
+            psk_mgmt=WPA-PSK
+    }" >> /etc/wpa_supplicant/wpa_supplicant.conf
+fi
