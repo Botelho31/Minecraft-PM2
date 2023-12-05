4f3060e6e143063f300e226c8314f7e96e2fa395
Botelho31 <lucas.vbotelho83@gmail.com>
2023-12-05 19:34:55 -0300

    feat: setup pi working

--- scripts/setup-pi.sh
+++ scripts/setup-pi.sh
@@ -4,3 +4,13 @@
 echo "usb-storage.quirks=152d:0578:u " > temp.txt
 cat /boot/cmdline.txt >> temp.txt
 sudo mv temp.txt /boot/cmdline.txt
+
+sudo echo `interface eth0
+static ip_address=192.168.0.9/24
+static routers=192.168.0.1
+static domain_name_servers=192.168.0.1 8.8.8.8
+
+interface wlan0
+static ip_address=192.168.0.9/24
+static routers=192.168.0.1
+static domain_name_servers=192.168.0.1 8.8.8.8` >> /etc/dhcpcd.conf
