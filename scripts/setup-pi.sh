d73ff6cc8852585826a1200e1fdd7d8639c586dd
Botelho31 <lucas.vbotelho83@gmail.com>
2023-12-05 20:00:53 -0300

    feat: change setup pi script

--- scripts/setup-pi.sh
+++ scripts/setup-pi.sh
@@ -9,7 +9,7 @@ if grep -q "usb-storage.quirks=152d:0578:u" /boot/cmdline.txt; then
     echo "usb-storage.quirks=152d:0578:u is already in the file"
 else
     echo "usb-storage.quirks=152d:0578:u is not in the file"
-    echo "usb-storage.quirks=152d:0578:u " > temp.txt
+    echo -n "usb-storage.quirks=152d:0578:u " > temp.txt
     cat /boot/cmdline.txt >> temp.txt
     sudo mv temp.txt /boot/cmdline.txt
 fi
@@ -20,29 +20,32 @@ echo "Done configuring SSD"
 echo "Configuring static ip and wifi..."
 if grep -q "192.168.0.9/24" /etc/dhcpcd.conf; then
     echo "The static ip is already in the file"
-else 
+else
     echo "The static ip is not in the file"
-    sudo echo `
-    interface eth0
-    static ip_address=192.168.0.9/24
-    static routers=192.168.0.1
-    static domain_name_servers=192.168.0.1 8.8.8.8
+        # Append the necessary configuration to /etc/dhcpcd.conf
+    sudo bash -c 'cat <<EOT >> /etc/dhcpcd.conf
+interface eth0
+static ip_address=192.168.0.9/24
+static routers=192.168.0.1
+static domain_name_servers=192.168.0.1 8.8.8.8
 
-    interface wlan0
-    static ip_address=192.168.0.9/24
-    static routers=192.168.0.1
-    static domain_name_servers=192.168.0.1 8.8.8.8` >> /etc/dhcpcd.conf
+interface wlan0
+static ip_address=192.168.0.9/24
+static routers=192.168.0.1
+static domain_name_servers=192.168.0.1 8.8.8.8
+EOT'
 fi
 
 if grep -q "RALU_NET_5G" /etc/wpa_supplicant/wpa_supplicant.conf; then
     echo "The wifi is already in the file"
-else 
+else
     echo "The wifi is not in the file"
-    sudo echo "
-    network={
-            ssid="RALU_NET_5G"
-            psk=0815233128
-            psk_mgmt=WPA-PSK
-    }" >> /etc/wpa_supplicant/wpa_supplicant.conf
+    sudo bash -c 'cat <<EOT >> /etc/wpa_supplicant/wpa_supplicant.conf
+network={
+    ssid="RALU_NET_5G"
+    psk="0815233128"
+    key_mgmt=WPA-PSK
+}
+EOT'
 fi
 echo "Done configuring static ip and wifi"
