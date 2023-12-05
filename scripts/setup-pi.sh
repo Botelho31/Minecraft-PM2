7c8c6f565bd9cc0af2ba0b07a9aa19bf0dcd7cc7
Botelho31 <lucas.vbotelho83@gmail.com>
2023-12-05 19:37:30 -0300

    feat: add break line and internet password

--- scripts/setup-pi.sh
+++ scripts/setup-pi.sh
@@ -5,7 +5,8 @@ echo "usb-storage.quirks=152d:0578:u " > temp.txt
 cat /boot/cmdline.txt >> temp.txt
 sudo mv temp.txt /boot/cmdline.txt
 
-sudo echo `interface eth0
+sudo echo `
+interface eth0
 static ip_address=192.168.0.9/24
 static routers=192.168.0.1
 static domain_name_servers=192.168.0.1 8.8.8.8
@@ -14,3 +15,10 @@ interface wlan0
 static ip_address=192.168.0.9/24
 static routers=192.168.0.1
 static domain_name_servers=192.168.0.1 8.8.8.8` >> /etc/dhcpcd.conf
+
+sudo echo "
+network={
+        ssid="RALU_NET_5G"
+        psk=0815233128
+        psk_mgmt=WPA-PSK
+}" >> /etc/wpa_supplicant/wpa_supplicant.conf
